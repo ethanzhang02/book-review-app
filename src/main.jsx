67045bf4c9e4efe3cef33ad467bc18b850365f50
Ethan Zhang <ez2262@columbia.edu>
2025-01-28 16:36:20 -0500

    Fixed main.jsx bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
+import React from 'react'; // Correct way to import React
+import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import App from './App.jsx'; // Adjust the path if needed
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode> {/* Correct usage of React.StrictMode */}
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
+
