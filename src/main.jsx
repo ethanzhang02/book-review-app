4c565ed86720766f36ab512f98a6df8a379ab842
Ethan Zhang <ez2262@columbia.edu>
2025-01-28 16:55:52 -0500

    added gunicorn requirements

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from 'react'; 
-import { createRoot } from 'react-dom/client'; 
-import App from './App.jsx'; 
+import React from 'react'; // Correct way to import React
+import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import App from './App.jsx'; // Adjust the path if needed
+
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode> {/* Correct usage of React.StrictMode */}
     <App />
   </React.StrictMode>,
 );
