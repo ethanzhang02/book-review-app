0afd6080eafe2ffdbef73bc0acdb243425171d9b
Ethan Zhang <ez2262@columbia.edu>
2025-01-29 20:44:03 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change the default output directory from 'dist' to 'build'
+  },
 })
